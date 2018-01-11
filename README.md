d514ec8052ff569e08e9d065935b4299463b9440
Tech-A04 <tech-a04@Tech-A04s-MacBook-Air.local>
2018-01-11 14:51:20 +0000

    all files updated

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # sparta-ruby-control-flow
+
+Task performed during class to know how to write loops using Ruby.
--- main.rb
+++ main.rb
@@ -1,35 +1,35 @@
 # IF STATEMENTS -------------------------
-# conditional = 11
-#
-# if conditional < 10
-#   puts 'hello'
-# end
-#
-#
-# if contidional < 10
-#   puts 'hello'
-# elsif conditional > 4
-#   puts 'thing'
-# else
-#   puts "something else"
-# end
+conditional = 11
+
+if conditional < 10
+  puts 'hello'
+end
+
+
+if contidional < 10
+  puts 'hello'
+elsif conditional > 4
+  puts 'thing'
+else
+  puts "something else"
+end
 
 
 # IF...THEN STATEMENTS--------------------
-# val = 2
-#
-# if (val == 1) then puts "1" else puts "Not 1" end
-#
-# # or
-#
-# if (val == 1)
-#   puts "1"
-# else
-#   puts "Not 1"
-# end
+val = 2
 
-# val = 1
-#
+if (val == 1) then puts "1" else puts "Not 1" end
+
+# or
+
+if (val == 1)
+  puts "1"
+else
+  puts "Not 1"
+end
+
+val = 1
+
 # puts val < 10 ? "it was true" : "it was false"
 #if value less than 10, print ...., else print.....
 
@@ -39,43 +39,51 @@
 
 # LOOPS , While...--------------------------
 
-# i = 0
-#
-# while i < 10 do
-#   puts "While: #{i}"
-#   i+=2
-# end
-#
+i = 0
+
+while i < 10 do
+  puts "While: #{i}"
+  i+=2
+end
+
+
+
 # # LOOPS , Do... While...--------------------------
-# i = 11
-#
-# begin
-#   puts "Do while: #{i}"
-#   i+=1
-# end while i < 10
+i = 11
+
+begin
+  puts "Do while: #{i}"
+  i+=1
+end while i < 10
+
+
 
 # LOOPS , Until...--------------------------
-# i = 0
-#
-# until i > 10 do
-#   puts "Until #{i}"
-#   i += 1
-# end
+i = 0
+
+until i > 10 do
+  puts "Until #{i}"
+  i += 1
+end
+
+
 
 # LOOPS , For...--------------------------
-# i = 0
-#
-# for i in 0..5
-#   puts "For loops are great: #{i}"
-# end
+i = 0
+
+for i in 0..5
+  puts "For loops are great: #{i}"
+end
+
+
 
 # LOOPS , Each...--------------------------
-#
-# i = 0
-#
-# (0..5).each do |i|
-#   puts "value of local variable is #{i}"
-# end
+
+i = 0
+
+(0..5).each do |i|
+  puts "value of local variable is #{i}"
+end
 
 
 #.MAP returns a new array with the values from the new original array
@@ -94,10 +102,12 @@ more_words = ['one', 'two', 'three']
 
 # puts more_words
 
+# ------------------------------------
+
 some_numbers = [1, 4, 5, 2, 3, 4, 7]
 sum = some_numbers.reduce 0 do |total, number|
   equal = total + number
   puts equal
   equal
 end
-# puts sum
+puts sum
